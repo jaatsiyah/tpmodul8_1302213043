7953f2a5313c98af5bb319bc2e8591f1fe819729
jaatsiyah <jaatsiyah@student.telkomuniversity.ac.id>
2023-04-09 20:26:54 +0700

    menyelesaikan program dengan runtime configuration

---
+++ tpmodul8_1302213043/tpmodul8_1302213043/CovidConfig.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Text.Json;
+
+public class CovidConfig
+{
+	public string satuan_suhu { get; set; }
+	public int batas_hari_demam { get; set; }
+	public string pesan_ditolak { get; set; }
+	public string pesan_diterima { get; set; }
+
+	public CovidConfig() { }
+
+	public CovidConfig(string satuan_suhu,int batas_hari_demam,string pesan_ditolak,string pesan_diterima)
+	{
+		this.satuan_suhu = satuan_suhu;
+		this.batas_hari_demam = batas_hari_demam;
+		this.pesan_ditolak = pesan_ditolak;
+		this.pesan_diterima = pesan_diterima;
+
+	}
+
+	public void UbahSatuan()
+	{
+		if (satuan_suhu == "celcius")
+		{
+			satuan_suhu = "fahrenheit";
+		} 
+		else 
+		{
+			satuan_suhu = "celcius";
+		}
+	}
+}
+
+class UIConfig
+{
+    public CovidConfig covidConfig;
+	public const string fileLocation = "D:\\TELKOM\\SEMESTER 4\\KONSTRUKSI PERANGKAT LUNAK\\tpmodul8_1302213043\\tpmodul8_1302213043\\tpmodul8_1302213043\\";
+	public const string filePath = fileLocation + "covid_config.json";
+
+	public UIConfig()
+	{
+		try
+		{
+			ReadConfigFile();
+		}
+		catch (Exception)
+		{
+			setDefault();
+			writeNewConfigFile();
+		}
+	}
+
+	private CovidConfig ReadConfigFile()
+	{
+		string configJSONData = File.ReadAllText(filePath);
+		covidConfig = JsonSerializer.Deserialize<CovidConfig>(configJSONData);
+		return covidConfig;
+	}
+
+	private void setDefault()
+	{
+		
+		string pesanDitolak = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+		string pesanDiterima = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
+
+		covidConfig = new CovidConfig("celcius", 14, pesanDitolak, pesanDiterima);
+	}
+
+	private void writeNewConfigFile()
+	{
+		JsonSerializerOptions options = new JsonSerializerOptions()
+		{
+			WriteIndented = true
+		};
+
+		string jsonString = JsonSerializer.Serialize(covidConfig, options);
+		File.WriteAllText(filePath, jsonString);
+	}
+}
+
+
---
+++ tpmodul8_1302213043/tpmodul8_1302213043/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        CovidConfig config = new CovidConfig();
+        UIConfig uiConfig = new UIConfig();
+
+        
+        uiConfig.covidConfig.UbahSatuan();
+
+        Console.WriteLine("Berapa suhu badan anda saat ini? Dalam nilai " +  uiConfig.covidConfig.satuan_suhu);
+        double suhu = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam ?");
+        int hari = int.Parse(Console.ReadLine());
+
+
+        if( hari < uiConfig.covidConfig.batas_hari_demam)
+        {
+            if (uiConfig.covidConfig.satuan_suhu == "celcius" && suhu >= 36.5 && suhu <= 37.5)
+            {
+                Console.WriteLine(uiConfig.covidConfig.pesan_diterima);
+
+            } 
+            else if (uiConfig.covidConfig.satuan_suhu == "fahrenheit" && suhu >= 97.7 && suhu <= 99.5)
+            {
+                Console.WriteLine(uiConfig.covidConfig.pesan_diterima);
+            }else
+            {
+                Console.WriteLine(uiConfig.covidConfig.pesan_ditolak);
+            }
+        }else
+        {
+            Console.WriteLine(uiConfig.covidConfig.pesan_ditolak);
+        }
+    }
+}
